440f4b007abad4597a06a872d3435d0386193cbb
Jorrit Salverda <jorrit.salverda@gmail.com>
2017-03-21 22:08:48 +0100

    mount docker.sock and docker binary

--- main.go
+++ main.go
@@ -54,9 +54,9 @@ func main() {
 			workingDirectory = p.WorkingDirectory
 		}
 
-		// run docker with image and steps from yaml
-		fmt.Printf("[Running command 'docker run --privileged --rm --entrypoint \"\" -v %v:%v -v /var/run:/var/run -v /volume/usr/local/bin:/volume/usr/local/bin -w %v %v %v -c %v']\n", dir, workingDirectory, workingDirectory, p.ContainerImage, shellPath, strings.Join(p.Commands, ";"))
-		cmd := exec.Command("docker", "run", "--privileged", "--rm", "--entrypoint", "", "-v", fmt.Sprintf("%v:%v", dir, workingDirectory), "-v", "/var/run:/var/run", "-v", "/usr/local/bin:/usr/local/bin", "-w", workingDirectory, p.ContainerImage, shellPath, "-c", strings.Join(p.Commands, ";"))
+		// run docker with image and commands from yaml
+		fmt.Printf("[Running command 'docker run --privileged --rm --entrypoint \"\" -v %v:%v -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker -w %v %v %v -c %v']\n", dir, workingDirectory, workingDirectory, p.ContainerImage, shellPath, strings.Join(p.Commands, ";"))
+		cmd := exec.Command("docker", "run", "--privileged", "--rm", "--entrypoint", "", "-v", fmt.Sprintf("%v:%v", dir, workingDirectory), "-v", "/var/run/docker.sock:/var/run/docker.sock", "-v", "/usr/bin/docker:/usr/bin/docker", "-w", workingDirectory, p.ContainerImage, shellPath, "-c", strings.Join(p.Commands, ";"))
 		stdout, err := cmd.StdoutPipe()
 		if err != nil {
 			log.Fatal(err)
