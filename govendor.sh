1073e0edc628918bce41f109104951c3b918c24a
Jorrit Salverda <jorrit.salverda@gmail.com>
2017-04-05 08:14:39 +0200

    added github.com/docker/docker/api/types/reference@v17.03.1-ce to vendor packages

--- govendor.sh
+++ govendor.sh
@@ -3,4 +3,5 @@ govendor fetch gopkg.in/yaml.v2
 govendor fetch github.com/stretchr/testify/assert@v1.1.4
 govendor fetch github.com/olekukonko/tablewriter
 govendor fetch github.com/docker/docker/api/types@v17.03.1-ce
+govendor fetch github.com/docker/docker/api/types/reference@v17.03.1-ce
 govendor fetch github.com/docker/docker/client@v17.03.1-ce
