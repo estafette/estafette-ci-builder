b47c2defb35fc8841c10f8bb8131bc344b8fcb8e
Jorrit Salverda <jsalverda@travix.com>
2017-08-16 10:56:14 +0200

    upgraded docker package to 17.05.0-ce (moby)

--- govendor.sh
+++ govendor.sh
@@ -2,6 +2,6 @@ govendor init
 govendor fetch gopkg.in/yaml.v2
 govendor fetch github.com/stretchr/testify/assert@v1.1.4
 govendor fetch github.com/olekukonko/tablewriter
-govendor fetch -tree github.com/docker/docker@v17.04.0-ce
+govendor fetch -tree github.com/moby/moby@v17.05.0-ce
 govendor fetch github.com/Knetic/govaluate@2.3.0
 govendor fetch github.com/rs/zerolog/log@v1.2.2
