71f4ff88f4382ca048e858898f214fcafbdcb078
Jorrit Salverda <jsalverda@travix.com>
2017-08-22 13:11:40 +0200

    add yaml.v2 package to vendor dir

--- govendor.sh
+++ govendor.sh
@@ -1,4 +1,5 @@
 govendor init
+govendor fetch gopkg.in/yaml.v2
 govendor fetch github.com/stretchr/testify/assert@v1.1.4
 govendor fetch github.com/stretchr/testify/mock@v1.1.4
 govendor fetch github.com/olekukonko/tablewriter
