b01bf227ada964640e84124bc2f503695939767f
Jorrit Salverda <jsalverda@travix.com>
2019-11-22 11:39:19 +0100

    use 15 for trapping sigterm

--- templates/estafette-entrypoint.sh
+++ templates/estafette-entrypoint.sh
@@ -4,11 +4,11 @@ set -e
 
 forward_sigterm() {
     echo "Received SIGTERM, forwarding to child processes..."
-    trap - TERM
+    trap - 15
     pkill -P $$
 }
 
-trap forward_sigterm TERM
+trap forward_sigterm 15
 
 {{range .Commands}}
     {{- .}} &
