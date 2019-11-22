500982d5e5f9caf349b25ec8230de5251adb9803
Jorrit Salverda <jsalverda@travix.com>
2019-11-22 05:23:51 +0100

    forward sigterm to child processes

--- templates/estafette-entrypoint.sh
+++ templates/estafette-entrypoint.sh
@@ -1,12 +1,14 @@
 #!{{.Shell}}
+
 set -e
 
 sigterm_handler() {
     echo "Received SIGTERM, forwarding to forked processes"
-    ps aux
+    trap - 15
+    kill -- -$$ # Sends SIGTERM to child/sub processes
 }
 
-trap 'kill ${!}; sigterm_handler' 15 # SIGTERM
+trap sigterm_handler 15
 
 {{range .Commands}}
     {{- .}}
