7624e2b8cce7580381ccb7e76c0ffe326709f00c
Jorrit Salverda <jsalverda@travix.com>
2019-11-23 15:59:13 +0100

    use echo -e for color codes

--- templates/estafette-entrypoint.sh
+++ templates/estafette-entrypoint.sh
@@ -2,11 +2,11 @@
 set -e
 
 {{- range .Commands}}
-echo "\x1b[1m> {{.}}\x1b[0m"
+echo -e "\x1b[1m> {{.}}\x1b[0m"
 {{.}} &
 trap "kill $!; wait; exit" 1 2 15
 wait
 {{- end}}
 
-echo "\x1b[1m> {{.FinalCommand}}\x1b[0m"
+echo -e "\x1b[1m> {{.FinalCommand}}\x1b[0m"
 exec {{.FinalCommand}}
