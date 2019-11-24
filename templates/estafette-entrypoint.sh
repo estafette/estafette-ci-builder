ef899177beb53a65aeb6b58a62a03f5e44ee2c2e
Jorrit Salverda <jsalverda@travix.com>
2019-11-24 12:01:03 +0100

    show in logs whether command runs in background or runs with exec

--- templates/estafette-entrypoint.sh
+++ templates/estafette-entrypoint.sh
@@ -2,15 +2,16 @@
 set -e
 
 {{- range .Commands }}
-echo -e "\x1b[38;5;250m> {{.Command}}\x1b[0m"
 {{- if .RunInBackground}}
+echo -e "\x1b[38;5;250m> {{.Command}} &\x1b[0m"
 {{.Command}} &
 trap "kill $!; wait; exit" 1 2 15
 wait
 {{- else }}
+echo -e "\x1b[38;5;250m> {{.Command}}\x1b[0m"
 {{.Command}}
 {{- end }}
 {{- end }}
 
-echo -e "\x1b[38;5;250m> {{.FinalCommand}}\x1b[0m"
+echo -e "\x1b[38;5;250m> exec {{.FinalCommand}}\x1b[0m"
 exec {{.FinalCommand}}
