e12e0fea043c3525459d814386f3ea658e742739
Jorrit Salverda <jsalverda@travix.com>
2021-06-03 08:50:41 +0200

    rename BuildEvent enum to BuildEventType

--- builder/end_of_life_helper.go
+++ builder/end_of_life_helper.go
@@ -215,18 +215,18 @@ func (elh *endOfLifeHelperImpl) SendBuildJobLogEventCore(ctx context.Context, bu
 
 func (elh *endOfLifeHelperImpl) SendBuildStartedEvent(ctx context.Context) error {
 	buildStatus := contracts.LogStatusRunning
-	return elh.sendBuilderEvent(ctx, buildStatus, fmt.Sprintf("builder:%v", buildStatus.ToStatus()), contracts.BuildEventUpdateStatus)
+	return elh.sendBuilderEvent(ctx, buildStatus, fmt.Sprintf("builder:%v", buildStatus.ToStatus()), contracts.BuildEventTypeUpdateStatus)
 }
 
 func (elh *endOfLifeHelperImpl) SendBuildFinishedEvent(ctx context.Context, buildStatus contracts.LogStatus) error {
-	return elh.sendBuilderEvent(ctx, buildStatus, fmt.Sprintf("builder:%v", buildStatus.ToStatus()), contracts.BuildEventUpdateStatus)
+	return elh.sendBuilderEvent(ctx, buildStatus, fmt.Sprintf("builder:%v", buildStatus.ToStatus()), contracts.BuildEventTypeUpdateStatus)
 }
 
 func (elh *endOfLifeHelperImpl) SendBuildCleanEvent(ctx context.Context, buildStatus contracts.LogStatus) error {
-	return elh.sendBuilderEvent(ctx, buildStatus, "builder:clean", contracts.BuildEventClean)
+	return elh.sendBuilderEvent(ctx, buildStatus, "builder:clean", contracts.BuildEventTypeClean)
 }
 
-func (elh *endOfLifeHelperImpl) sendBuilderEvent(ctx context.Context, buildStatus contracts.LogStatus, event string, buildEvent contracts.BuildEvent) (err error) {
+func (elh *endOfLifeHelperImpl) sendBuilderEvent(ctx context.Context, buildStatus contracts.LogStatus, event string, buildEventType contracts.BuildEventType) (err error) {
 
 	span, ctx := opentracing.StartSpanFromContext(ctx, "SendBuildStatus")
 	defer span.Finish()
@@ -242,12 +242,12 @@ func (elh *endOfLifeHelperImpl) sendBuilderEvent(ctx context.Context, buildStatu
 		var requestBody io.Reader
 
 		ciBuilderEvent := contracts.EstafetteCiBuilderEvent{
-			BuildEvent: buildEvent,
-			JobType:    elh.config.JobType,
-			Build:      elh.config.Build,
-			Release:    elh.config.Release,
-			Bot:        elh.config.Bot,
-			Git:        elh.config.Git,
+			BuildEventType: buildEventType,
+			JobType:        elh.config.JobType,
+			Build:          elh.config.Build,
+			Release:        elh.config.Release,
+			Bot:            elh.config.Bot,
+			Git:            elh.config.Git,
 
 			JobName: jobName,
 			PodName: elh.podName,
