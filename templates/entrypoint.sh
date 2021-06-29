a5cfa10b1e3ef7a0803a4c8b00cf092ccabb7b81
Jorrit Salverda <jsalverda@travix.com>
2021-06-29 09:28:26 +0200

    use printf '%s\n' to render command to avoid 'printf: %40t: invalid directive' error

--- templates/entrypoint.sh
+++ templates/entrypoint.sh
@@ -3,21 +3,21 @@ set -e
 
 {{- range .Commands }}
 {{- if .RunInBackground}}
-printf "\033[38;5;250m> {{.EscapedCommand}} &\033[0m"
+printf '%s\n' "\033[38;5;250m> {{.EscapedCommand}} &\033[0m"
 {{.Command}} &
 trap "kill $!; wait; exit" 1 2 15
 wait $!
 {{- else }}
-printf "\033[38;5;250m> {{.EscapedCommand}}\033[0m"
+printf '%s\n' "\033[38;5;250m> {{.EscapedCommand}}\033[0m"
 {{.Command}}
 {{- end }}
 {{- end }}
 {{- if .RunFinalCommandWithExec}}
 
-printf "\033[38;5;250m> exec {{.EscapedFinalCommand}}\033[0m"
+printf '%s\n' "\033[38;5;250m> exec {{.EscapedFinalCommand}}\033[0m"
 exec {{.FinalCommand}}
 {{- else }}
 
-printf "\033[38;5;250m> {{.EscapedFinalCommand}}\033[0m"
+printf '%s\n' "\033[38;5;250m> {{.EscapedFinalCommand}}\033[0m"
 {{.FinalCommand}}
 {{- end }}
