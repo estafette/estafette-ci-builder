f5cbd8790a92acc2e5d896b19a2bebd2b3fbe441
Aleksandar Ivanov <alex.arcanum@gmail.com>
2022-12-30 13:34:01 +0100

    add errors to stage build env vars

--- pkg/builder/pipeline_runner.go
+++ pkg/builder/pipeline_runner.go
@@ -62,8 +62,12 @@ func (pr *pipelineRunner) RunStage(ctx context.Context, depth int, dir string, e
 	// init some variables
 	parentStageName, stagePlaceholder, autoInjected := pr.initStageVariables(ctx, depth, dir, envvars, parentStage, stage)
 	stage.ContainerImage = os.Expand(stage.ContainerImage, pr.envvarHelper.getEstafetteEnv)
-	_ = pr.envvarHelper.setEstafetteEnv("ESTAFETTE_STAGE_BUILD_REVISION", pr.applicationInfo.Revision)
-	_ = pr.envvarHelper.setEstafetteEnv("ESTAFETTE_STAGE_BUILD_BUILD_DATE", pr.applicationInfo.BuildDate)
+	if err = pr.envvarHelper.setEstafetteEnv("ESTAFETTE_STAGE_BUILD_REVISION", pr.applicationInfo.Revision); err != nil {
+		log.Logger.Error().Msgf("env var ESTAFETTE_STAGE_BUILD_REVISION could not be set: %v", err)
+	}
+	if err = pr.envvarHelper.setEstafetteEnv("ESTAFETTE_STAGE_BUILD_BUILD_DATE", pr.applicationInfo.BuildDate); err != nil {
+		log.Logger.Error().Msgf("env var ESTAFETTE_STAGE_BUILD_BUILD_DATE could not be set: %v", err)
+	}
 
 	log.Debug().Msgf("%v Starting stage", stagePlaceholder)
 
